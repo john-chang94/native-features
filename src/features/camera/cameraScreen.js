629f726744c130ad82b0d0cc963d13aec91ac08e
john-chang94 <j.chang94@yahoo.com>
2021-12-14 18:28:11 -0800

    android camera preview fix

--- src/features/camera/cameraScreen.js
+++ src/features/camera/cameraScreen.js
@@ -1,15 +1,21 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
-import { Avatar } from "react-native-paper";
+import { StyleSheet, View, Text, TouchableOpacity, Dimensions } from "react-native";
 import { Camera } from "expo-camera";
 import { ImageContext } from "../../contexts/imageContext";
 import { Ionicons } from "@expo/vector-icons";
+import { isAndroid } from "../../utils/sysChecks";
 
 export default function CameraScreen({ navigation }) {
   const [hasPermission, setHasPermission] = useState(false);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const { setImage } = useContext(ImageContext);
   const cameraRef = useRef();
+
+  const [imagePadding, setImagePadding] = useState(0);
+  const [ratio, setRatio] = useState('4:3');  // default is 4:3
+  const { height, width } = Dimensions.get('window');
+  const screenRatio = height / width;
+  const [isRatioSet, setIsRatioSet] =  useState(false);
 
   const getPermission = async () => {
     const { status } = await Camera.requestCameraPermissionsAsync();
@@ -32,6 +38,55 @@ export default function CameraScreen({ navigation }) {
     }
   };
 
+  // [ANDROID]
+  const prepareRatio = async () => {
+    let desiredRatio = ratio;
+    if (isAndroid) {
+      const ratios = await cameraRef.current.getSupportedRatiosAsync();
+
+      // Calculate the width/height of each of the supported camera ratios
+      // These width/height are measured in landscape mode
+      // Find the ratio that is closest to the screen ratio without going over
+      let distances = {};
+      let realRatios = {};
+      let minDistance = null;
+      for (const ratio of ratios) {
+        const parts = ratio.split(':');
+        const realRatio = parseInt(parts[0]) / parseInt(parts[1]);
+        realRatios[ratio] = realRatio;
+        // Ratio can't be taller than screen, so we don't want an abs()
+        const distance = screenRatio - realRatio; 
+        distances[ratio] = realRatio;
+        if (minDistance == null) {
+          minDistance = ratio;
+        } else {
+          if (distance >= 0 && distance < distances[minDistance]) {
+            minDistance = ratio;
+          }
+        }
+      }
+      // Set the best match
+      desiredRatio = minDistance;
+      //  Calculate the difference between the camera width and the screen height
+      const remainder = Math.floor(
+        (height - realRatios[desiredRatio] * width) / 2
+      );
+      // Set the preview padding and preview ratio
+      setImagePadding(remainder);
+      setRatio(desiredRatio);
+      // Set a flag so we don't do this 
+      // calculation each time the screen refreshes
+      setIsRatioSet(true);
+    }
+  };
+
+  // Camera must be loaded in order to access the supported ratios
+  const setCameraReady = async() => {
+    if (!isRatioSet) {
+      await prepareRatio();
+    }
+  };
+
   useEffect(() => {
     getPermission();
   }, []);
@@ -43,8 +98,10 @@ export default function CameraScreen({ navigation }) {
   return (
     <Camera
       ref={(camera) => (cameraRef.current = camera)}
-      style={styles.container}
+      style={[styles.container, { marginTop: imagePadding, marginBottom: imagePadding }]}
       type={type}
+      onCameraReady={setCameraReady}
+      ratio={isAndroid && ratio}
     >
       <View style={styles.innerContainer}>
         <TouchableOpacity style={styles.button} onPress={() => flipCamera()}>
@@ -53,7 +110,10 @@ export default function CameraScreen({ navigation }) {
         <TouchableOpacity style={styles.button} onPress={() => snap()}>
           <Ionicons name="ellipse-outline" size={32} />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
           <Ionicons name="close" size={32} />
         </TouchableOpacity>
       </View>
@@ -65,6 +125,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-end",
+    // marginTop: isAndroid ? imagePadding : 0,
+    // marginBottom: isAndroid ? imagePadding : 0
   },
   innerContainer: {
     height: 125,
